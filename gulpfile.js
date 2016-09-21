a728217dcb740e646bdb7d8d0142c6dfe94eb622
Jesse Breneman <jmwashere@gmail.com>
2016-09-20 23:41:11 -0400

    scaffold html out

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	rename = require('gulp-rename'),
 	autoprefixer = require('gulp-autoprefixer'),
-	babel = require('gulp-babel');
+	babel = require('gulp-babel'),
+	browserSync = require('browser-sync').create();
 
 var dirs = {
 	src: './src/',
@@ -11,11 +12,12 @@ var dirs = {
 
 // scss building
 gulp.task('sass', function(){
-	return gulp.src(dirs.src + 'scss/*.scss')
+	return gulp.src(dirs.src + 'css/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer())
 		.pipe(rename('app.min.css'))
-		.pipe(gulp.dest(dirs.build + 'css/'));
+		.pipe(gulp.dest(dirs.build + 'css/'))
+		.pipe(browserSync.stream());
 });
 
 // js transpilation
@@ -25,10 +27,18 @@ gulp.task('js', function(){
             presets: ['es2015']
         }))
 		.pipe(rename('app.min.js'))
-		.pipe(gulp.dest(dirs.build + 'css/'));
+		.pipe(gulp.dest(dirs.build + 'css/'))
+		.pipe(browserSync.stream());
 });
 
 gulp.task('default', ['sass', 'js'], function() {
-	gulp.watch(dirs.src + 'scss/**/*.scss', ['sass']);
+	browserSync.init({
+        server: "./public"
+    });
+
+	gulp.watch(dirs.src + 'css/**/*.scss', ['sass']);
 	gulp.watch(dirs.src + 'js/**/*.js', ['js']);
+	gulp.watch('./public' + '**/*.html', function() {
+		browserSync.reload();
+	});
 });
